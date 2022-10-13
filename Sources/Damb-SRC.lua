a2a6f0736e507c8e031ab6c68048d27e7389aa34
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-10-13 16:18:06 -0400

    Update Damb-SRC.lua

--- Sources/Damb-SRC.lua
+++ Sources/Damb-SRC.lua
@@ -4,6 +4,42 @@ local PlayerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui
 
 function Library:Window(options)
 	local gui = {}
+	function SmoothDrag(SmoothDragObject)
+	    local UIS = game:GetService("UserInputService")
+            local frame = SmoothDragObject
+
+	 	local dragToggle = nil
+		local dragSpeed = 0.1
+		local dragStart = nil
+		local startPos = nil
+
+		local function updateInput(input)
+			local delta = input.Position - dragStart
+			local position = UDim2.new(startPos.X.Scale,startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+			game:GetService("TweenService"):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+		end
+
+		frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
+		dragToggle = true
+		dragStart = input.Position
+		startPos = frame.Position
+		input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+
+		UIS.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				if dragToggle then
+					updateInput(input)
+					end
+				end
+			end)
+		end
 	do
 		-- StarterGui.ScreenGui
 		gui["1"] = Instance.new("ScreenGui", runService:IsStudio() and PlayerGui or game.CoreGui);
@@ -266,10 +302,7 @@ function Library:Window(options)
 			return Notification
 		end
 		
-		do
-		   loadstring(game:HttpGet("https://raw.githubusercontent.com/danielgamer9799/my-stupid-ui-libraries/main/Important%20Features/Smooth%20Drag.lua"))()
-	           --smooth drag
-		end
+		SmoothDrag(gui["2"])
 		
 		return gui
 	end
